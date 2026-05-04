19e44bd5c198a3aeef0d98c0ad4f77d062971a1d
Vladimir Rostov <info@veter64.ru>
2026-05-04 11:38:22 +0400

    Сделал интерфейс осталось с проблемы с пакедж менеджером

--- Assets/Scripts/NewFromAI/MobilePlatformDetector.cs
+++ Assets/Scripts/NewFromAI/MobilePlatformDetector.cs
@@ -4,11 +4,11 @@ using UnityEngine.UI;
 public class MobilePlatformDetector : MonoBehaviour
 {
     public GameObject mobileUI;
-    public Text TextPlatform;
+   // public Text TextPlatform;
     public MonoBehaviour mobileMovement;
 
-    private string _mob = "Mobile";
-    private string _pc = "PC";
+    //private string _mob = "Mobile";
+    //private string _pc = "PC";
 
 #if UNITY_EDITOR
     [Tooltip("Принудительно эмулировать мобильное устройство в редакторе")]
@@ -20,50 +20,30 @@ public class MobilePlatformDetector : MonoBehaviour
         bool isMobile;
 
 #if UNITY_EDITOR
+        // В редакторе управляем вручную
         isMobile = simulateMobile;
 #else
-        // В билде определяем надёжным способом: Unity API + (если WebGL) плагин JavaScript
-        isMobile = Application.isMobilePlatform || IsMobileViaWebGLPlugin();
+        // В билде – проверка Unity (точно работает в WebGL на телефонах)
+        isMobile = Application.isMobilePlatform;
 #endif
 
-        Debug.Log($"Is Mobile: {isMobile} | DeviceType: {SystemInfo.deviceType} | Platform: {Application.platform}");
+        Debug.Log($"Is Mobile: {isMobile} | DeviceType: {SystemInfo.deviceType}");
 
         if (isMobile)
         {
             if (mobileUI != null) mobileUI.SetActive(true);
             if (mobileMovement != null) mobileMovement.enabled = true;
-            TextPlatform.text = _mob;
-            Debug.Log("Запуск как МОБИЛЬНОЕ устройство");
+            //TextPlatform.text = _mob;
+            //Debug.Log("Запуск как МОБИЛЬНОЕ устройство");
         }
         else
         {
             if (mobileUI != null) mobileUI.SetActive(false);
             if (mobileMovement != null) mobileMovement.enabled = false;
-            TextPlatform.text = _pc;
-            Debug.Log("Запуск как ДЕСКТОП");
+            //TextPlatform.text = _pc;
+            //Debug.Log("Запуск как ДЕСКТОП");
         }
     }
-
-#if !UNITY_EDITOR && UNITY_WEBGL
-    // Импорт нашей JavaScript-функции из плагина
-    [System.Runtime.InteropServices.DllImport("__Internal")]
-    private static extern int IsMobile();
-
-    private bool IsMobileViaWebGLPlugin()
-    {
-        try
-        {
-            return IsMobile() == 1;
-        }
-        catch
-        {
-            // Если вызов не удался (например, не WebGL), вернём false
-            return false;
-        }
-    }
-#else
-    private bool IsMobileViaWebGLPlugin() => false;
-#endif
 }
 
 
